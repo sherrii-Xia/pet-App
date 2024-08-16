c14a9e7067717a34bed31c0b3535b07a8cb5c34d
sherrii-Xia <2629172636@qq.com>
2024-08-16 14:55:28 +0800

    added color themes

--- demo-side-bar/app/_components/Header.tsx
+++ demo-side-bar/app/_components/Header.tsx
@@ -1,5 +1,5 @@
 function Header() {
-  return <div className="w-full h-12 bg-slate-500">this is the header, we need a search bar </div>;
+  return <div className="bg-secondary w-full h-12 ">this is the header, we need a search bar </div>;
 }
 
 export default Header;
--- demo-side-bar/tailwind.config.ts
+++ demo-side-bar/tailwind.config.ts
@@ -13,6 +13,25 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: {
+          DEFAULT: "#22577a", // Sidebar background blue
+          dark: "#043658", // Darker variant for hover effects
+        },
+        secondary: {
+          DEFAULT: "#38A3A5", // Main background light gray
+          light: "#57CC99", // Cards background
+          dark: "#374151", // Text color for dark elements
+        },
+        accent: {
+          green: "#80ED99", // Accent color for stats, e.g. "Active Customers"
+          red: "#C7F9CC", // Accent color for alerts or negative indicators
+        },
+        text: {
+          DEFAULT: "#1F2937", // Default text color (dark gray)
+          light: "#F9FDF9", // Light text color
+        },
+      },
     },
   },
   plugins: [],
