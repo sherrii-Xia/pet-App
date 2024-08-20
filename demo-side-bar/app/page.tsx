9c8d2e663e815f96c076b2d9a7c0a47276745ef9
sherrii-Xia <2629172636@qq.com>
2024-08-20 16:46:36 +0800

    added react query

--- demo-side-bar/app/page.tsx
+++ demo-side-bar/app/page.tsx
@@ -1,13 +1,23 @@
 import Image from "next/image";
 import { SidebarDemo } from "./_components/SidebarDemo";
 import MainContent from "./_components/MyDashboard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 export default function Home() {
   return (
     <main className="flex-1">
-
-    <MainContent/>  
-
-
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <MainContent />
+      </QueryClientProvider>
     </main>
   );
 }
