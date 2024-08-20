34b51403497d159ba971929e63ad598a2392233c
sherrii-Xia <2629172636@qq.com>
2024-08-20 15:48:25 +0800

    added dep @ subpase and build data connection

---
+++ demo-side-bar/services/apiPetServices.ts
@@ -0,0 +1,11 @@
+import supabase from "./supbase";
+
+export async function getPetService() {
+  const { data, error } = await supabase.from("PetServices").select("*");
+
+  if (error) {
+    console.log(error);
+    throw new Error("Failed to get Pet Services");
+  }
+  return data;
+}
